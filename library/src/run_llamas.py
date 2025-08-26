596fe1650955003e0d331da6fc513d39e5c8ac3b
John Tyler <jetyler@ualberta.ca>
2025-08-26 10:39:45 -0600

    updated python script

--- library/src/run_llamas.py
+++ library/src/run_llamas.py
@@ -3,13 +3,11 @@ import os
 
 def main(argv):
     verify = False
-    if argv[2] == 'yes':
+    if len(argv) > 2 and argv[2] == 'yes':
         verify = True
 
-    args = ["larft-example"]
-
     for matrix_file, special_file in zip(os.listdir(argv[0]), os.listdir(argv[1])):
-        args = ["larft-example", os.join(argv[0], matrix_file), os.path.join(argv[1], special_file)]
+        args = ["larft-example", os.path.join(argv[0], matrix_file), os.path.join(argv[1], special_file)]
         if verify:
             args.append('yes')
         try:
